a8547a7d103b422534f3816d4c47a017d8a38b87
Ben Fishbein <ben@hackillinois.org>
2014-09-03 15:27:00 -0400

    Some ocd whitespace stuff

--- www/index/Handlers.py
+++ www/index/Handlers.py
@@ -1,14 +1,17 @@
 from www.base.BaseHandler import BaseHandler
 import urllib, logging
 from db.Email import Email
+import re
 
 class MainHandler(BaseHandler):
     def get(self):
         self.render("index/default.html")
     def post(self):
-		#TODO: verify email in db
         email = str(urllib.unquote(self.request.get('email')))
-        if(Email.add({"email":email})):
-            self.write("success");
+		#TODO: render pages
+        if not re.match(r"^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$", email):
+            self.write("invalid")
+        elif(Email.add({"email":email})):
+            self.write("success")
         else:
             self.write("indatabase")
