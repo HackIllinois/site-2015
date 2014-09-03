94d7b96617547bb3b84ca1c2878f3bf35f58ffbd
Ben Fishbein <ben@hackillinois.org>
2014-09-02 23:27:59 -0400

    restructures error handling

--- www/handlers.py
+++ www/handlers.py
@@ -1,17 +1,16 @@
 from webapp2_extras.routes import RedirectRoute
 
 import www.index.Handlers
-import www.e401.Handlers
-import www.e404.Handlers
+import www.error.Handlers
 import www.base.LogoutHandler
 
 site_handlers = [
     RedirectRoute('/', handler=www.index.Handlers.MainHandler, name='Index', strict_slash=True),
     RedirectRoute('/logout', handler=www.base.LogoutHandler.LogoutHandler, name='Logout', strict_slash=True),
-    RedirectRoute('/404', handler=www.e404.Handlers.Error404Handler, name='Error404', strict_slash=True),
+    RedirectRoute('/404', handler=www.error.Handlers.Error404Handler, name='Error404', strict_slash=True),
 ]
 
 error_handlers = {
-    401: www.e401.Handlers.handle401,
-    404: www.e404.Handlers.handle404,
+    401: www.error.Handlers.handle401,
+    404: www.error.Handlers.handle404,
 }
