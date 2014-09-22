875145971030a848d7ffd479ed236b4dbd18ba9a
Ben Fishbein <Ben@hackillinois.org>
2014-09-22 15:34:17 -0400

    waves might actually work now

--- js/index/waves.js
+++ js/index/waves.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
     var scene = $('#scene')[0];
     var parallax = new Parallax(scene);
+
+    var resizeWaves = function() {
+        // Get aspect ratio of window
+        var aspectRatio = $(this).height() / $(this).width();
+        if (aspectRatio > 1.0) {
+            // Page is horizontal
+            $("#waves").css({
+                width: aspectRatio*100 + 50 + "%",
+                height: "auto"
+            });
+        } else {
+            $("#waves").css({
+                width: "140%",
+                height: "auto"
+            });
+        }
+    }
+    
+    $(window).resize(resizeWaves).ready(resizeWaves);
 });
