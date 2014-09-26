7e57525c1b07781384756972d8f26f6fa7beee0a
Ben Fishbein <Ben@hackillinois.org>
2014-09-26 08:32:25 -0400

    clean up directory

--- www/base/LogoutHandler.py
+++
@@ -1,7 +0,0 @@
-from www.base.BaseHandler import BaseHandler
-import urllib
-from google.appengine.api import users
-
-class LogoutHandler(BaseHandler):
-    def get(self):
-        self.redirect(users.create_logout_url("/"))
