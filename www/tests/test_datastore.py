c6ac91387cd25cea6c272aa7fbbbe5017fe7d3b0
Ben Fishbein <Ben@hackillinois.org>
2014-10-15 18:35:36 -0400

    removed old testing

--- www/tests/test_datastore.py
+++
@@ -1,9 +0,0 @@
-import random
-import unittest
-from classes import DatastoreTestCase
-
-class test_tests(DatastoreTestCase):
-
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        self.assertEqual(1,1)
